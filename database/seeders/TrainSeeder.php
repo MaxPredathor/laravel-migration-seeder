7b9a9a7119ae49687200910d3b3d0e0f583fcc16
MaxPredathor <max.gilli98@gmail.com>
2023-12-21 16:05:12 +0100

    ticket

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -21,11 +21,11 @@ class TrainSeeder extends Seeder
 
             $train = new Train();
             $train->company = $faker->company();
-            $train->departure_station = $faker->word();
-            $train->arrival_station = $faker->word();
+            $train->departure_station = $faker->city();
+            $train->arrival_station = $faker->city();
             $train->departure_time = $faker->dateTime();
             $train->arrival_time = $faker->dateTime();
-            $train->train_code = $faker->tinyInteger();
+            $train->train_code = $faker->barcode();
             $train->carriages = $faker->numberBetween(1, 20);
             $train->on_time = $faker->boolean();
             $train->cancelled = $faker->boolean();
